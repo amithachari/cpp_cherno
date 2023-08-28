cebc4d231e7481aaceabdfd4e2ba8570902cc43f
amithachari29 <amithachari29@gmail.com>
2023-08-28 01:05:11 -0500

    Arrow operator

--- cpp_cherno.cpp
+++ cpp_cherno.cpp
@@ -1,46 +1,34 @@
 #include<iostream>
 #include <cstring>
-class String{
-    private:
-        char* m_Buffer;
-        unsigned int m_Size;
+
+class Entity{
     public:
-        String(const char* string){
-            m_Size = strlen(string);
-            m_Buffer = new char[m_Size];
-            memcpy(m_Buffer, string, m_Size+1);
-        }
+        void Print() const { std::cout << "Hello " << std::endl;}
 
-        String(const String& other): m_Size(other.m_Size){
-            std::cout << "Copied String " << std::endl;
-            m_Buffer = new char[m_Size + 1];
-            memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
-        }
 
-        ~String(){
-            delete[] m_Buffer;
-        }
-
-        char& operator[](unsigned int index){
-            return m_Buffer[index];
-        }
-        friend std::ostream& operator << (std::ostream& stream, const String& string);
 };
 
-std::ostream& operator << (std::ostream& stream, const String& string){
-    stream << string.m_Buffer;
-    return stream;
-}
+class ScopedPtr{
+private:
+    Entity* m_Obj;
+public:
+    ScopedPtr(Entity* entity):m_Obj(entity)
+    {
+    }
+    ~ScopedPtr(){
+        delete m_Obj;
+    }
 
-void PrintString(const String& string){
-    std::cout << string << std::endl;
-}
-
-int main(){
-    String string = "Cherno";
-    String second = string;
-    second[2] = 'a';
-    PrintString(string);
-    PrintString(second);
+    Entity* operator->(){
+        return m_Obj;
+    }
+};
+int main()
+{    
+    Entity* e = new Entity;
+    (*e).Print();
+    e->Print();
+    ScopedPtr entity = new Entity;
+    entity->Print();
     return 0;
 }
